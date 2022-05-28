7718cefaa1d065c3c24000e2bf7c0ce3be0fbda8
SinjulMSBH <sinjul.msbh@yahoo.com>
2022-05-28 16:37:16 +0200

    Service

---
+++ Mc2.CrudTest.Services/Concretes/CustomerService.cs
@@ -0,0 +1,55 @@
+﻿using Mc2.CrudTest.Data.EF.DatabaseContext;
+using Mc2.CrudTest.Data.EF.Repositories.Concretes;
+using Mc2.CrudTest.Domain.Entities;
+using Mc2.CrudTest.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using PhoneNumbers;
+
+namespace Mc2.CrudTest.Services.Concretes
+{
+    public class CustomerService : EFRepository<Customer>, ICustomerService
+    {
+        public CustomerService(AppDbContext context) : base(context)
+        {
+        }
+
+        public async ValueTask<string?> GetPhoneNumberAsync(string phoneNumberRaw, string countryCodeSelected)
+        {
+            try
+            {
+                var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+
+                //var regCode = phoneNumberUtil.GetRegionCodeForCountryCode(44);
+
+                PhoneNumber queryPhoneNumber = phoneNumberUtil.Parse(phoneNumberRaw, countryCodeSelected);
+
+                if (phoneNumberUtil.IsValidNumber(queryPhoneNumber))
+                {
+                    var resultPhoneNumber = queryPhoneNumber.NationalNumber.ToString();
+
+                    return await Task.FromResult(resultPhoneNumber);
+                }
+
+                return default;
+            }
+            catch (NumberParseException npex)
+            {
+                throw new NumberParseException(npex.ErrorType, npex.Message);
+            }
+        }
+
+        public async ValueTask<Customer[]> GetCustomersAsync(CancellationToken cancellationToken = default)
+        {
+            var Customers1 = await GetAllAsync(cancellationToken);
+
+            var Customers2 = await Context.Customers.ToListAsync(cancellationToken);
+
+            var customers3 = await Entities
+                .AsNoTrackingWithIdentityResolution()
+                .ToArrayAsync(cancellationToken)
+            ;
+
+            return customers3;
+        }
+    }
+}
---
+++ Mc2.CrudTest.Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using Mc2.CrudTest.Data.EF.Repositories.Interfaces;
+using Mc2.CrudTest.Domain.Entities;
+using PhoneNumbers;
+
+namespace Mc2.CrudTest.Services.Interfaces;
+
+public interface ICustomerService : IEFRepository<Customer>
+{
+    ValueTask<Customer[]> GetCustomersAsync(CancellationToken cancellationToken = default);
+
+    ValueTask<string?> GetPhoneNumberAsync(string phoneNumberRaw, string countryCodeSelected);
+}
