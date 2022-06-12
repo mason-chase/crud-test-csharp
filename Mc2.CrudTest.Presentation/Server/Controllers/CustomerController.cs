c8e9a0f5f7cd80716a10b27c7dca516f48e27589
Taher <taherfattahi11@gmail.com>
2022-06-12 05:09:21 +0430

    implementing CustomerController and add all dependeny injection in startup.cs and add swagger to project

---
+++ Mc2.CrudTest.Presentation/Server/Controllers/CustomerController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using c2.CrudTest.Application.Commands;
+using c2.CrudTest.Application.Queries;
+
+namespace Mc2.CrudTest.Presentation.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+
+        [HttpGet]
+        [Route("GetCustomers")]
+        public async Task<IActionResult> GetCustomers()
+        {
+            return Ok(await Mediator.Send(new GetAllCustomersQuery()));
+        }
+
+        [HttpGet]
+        [Route("GetCustomer")]
+        public async Task<IActionResult> GetCustomerById(int customerId)
+        {
+            return Ok(await Mediator.Send(new GetCustomerByIdQuery { Id = customerId }));
+
+        }
+
+        [HttpPost]
+        [Route("AddCustomer")]
+        public async Task<IActionResult> AddCustomer(CreateCustomerCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPut]
+        [Route("UpdateCustomer")]
+        public async Task<IActionResult> UpdateCustomer(int customerId, UpdateCustomerCommand command)
+        {
+            if (customerId != command.Id)
+            {
+                return BadRequest();
+            }
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpDelete]
+        [Route("DeleteCustomer")]
+        public async Task<IActionResult> DeleteCustomer(int customerId)
+        {
+            return Ok(await Mediator.Send(new DeleteCustomerByIdCommand { Id = customerId }));
+        }
+
+    }
+}
--- Mc2.CrudTest.Presentation/Server/Startup.cs
+++ Mc2.CrudTest.Presentation/Server/Startup.cs
@@ -3,6 +3,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using MediatR;
+using c2.CrudTest.Application.CommandHandler;
+using c2.CrudTest.Application.Commands;
+using Mc2.CrudTest.Infrastructure;
+using c2.CrudTest.Application.QueriesHandler;
+using c2.CrudTest.Application.Queries;
+using System.Collections.Generic;
+using Mc2.CrudTest.Domain.Models.Entities;
+using c2.CrudTest.Application.Validators;
+using FluentValidation;
 
 namespace Mc2.CrudTest.Presentation.Server
 {
@@ -12,16 +24,35 @@ namespace Mc2.CrudTest.Presentation.Server
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddCors(option => option.AddPolicy("MyCrudPolicy", builder => {
+            //    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            //}));
 
-            services.AddControllersWithViews();
+            services.AddControllers();
             services.AddRazorPages();
+
+            services.AddPersistence(Configuration);
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient<IRequestHandler<CreateCustomerCommand, bool>, CreateCustomerCommandHandler>();
+            services.AddTransient<IRequestHandler<DeleteCustomerByIdCommand, int>, DeleteCustomerByIdCommandHandler>();
+            services.AddTransient<IRequestHandler<UpdateCustomerCommand, CustomerEntity>, UpdateCustomerCommandHandler>();
+            services.AddTransient<IRequestHandler<GetAllCustomersQuery, IEnumerable<CustomerEntity>>, GetAllCustomersQueryHandler>();
+            services.AddTransient<IRequestHandler<GetCustomerByIdQuery, CustomerEntity>, GetCustomerByIdQueryHandler>();
+
+            services.AddTransient<IValidator<CustomerEntity>, CustomerFluentValidation>();
+
+            services.AddSwaggerGen(c =>
+            {
+                //c.IncludeXmlComments(string.Format(@"{0}\CQRS.WebApi.xml", System.AppDomain.CurrentDomain.BaseDirectory));
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mc2.CrudTest.Presentation.Server", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,12 +62,15 @@ namespace Mc2.CrudTest.Presentation.Server
             {
                 app.UseDeveloperExceptionPage();
                 app.UseWebAssemblyDebugging();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mc2.CrudTest.Presentation.Server v1"));
             }
             else
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             app.UseHttpsRedirection();
