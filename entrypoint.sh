4b060eaca605c2f74148d1ed71854082ed3a7880
Taher Fattahi <taherfattahi11@gmail.com>
2022-06-13 01:19:23 +0430

    add --startup-project in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 run_cmd="dotnet watch run --project ./Mc2.CrudTest.Presentation/Server/ --urls http://0.0.0.0:5000"
 
-until /root/.dotnet/tools/dotnet-ef --project ./Mc2.CrudTest.Persistence/ database update --context CustomerDbContext; do
+until /root/.dotnet/tools/dotnet-ef --project ./Mc2.CrudTest.Persistence/ --startup-project ./Mc2.CrudTest.Presentation/Server/ database update --context CustomerDbContext; do
 >&2 echo "SQL Server is starting up"
 sleep 1
 done
