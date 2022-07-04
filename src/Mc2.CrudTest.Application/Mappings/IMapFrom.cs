509c1e3353e2798f814b43bb82b392ba9eeb161b
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-07-05 02:28:06 +0430

    Adding some custom exceptions, automapper default profile and registering mediateR, FluentValidation, Automapper in ioc container

---
+++ src/Mc2.CrudTest.Application/Mappings/IMapFrom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AutoMapper;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Mc2.CrudTest.Application.Mappings
+{
+    public interface IMapFrom<T>
+    {
+        void Mapping(Profile profile)
+        {
+            profile.CreateMap(typeof(T), GetType());
+        }
+    }
+}
