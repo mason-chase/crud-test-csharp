872f93de34e2c47ecdccd0ecabb1623eaeda4a87
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-07-05 14:11:56 +0430

    Completing delete customer command

--- src/Mc2.CrudTest.Application/Administration/Customers/Commands/DeleteCustomerCommand.cs
+++ src/Mc2.CrudTest.Application/Administration/Customers/Commands/DeleteCustomerCommand.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using MediatR;
+using System.Threading;
 using System.Threading.Tasks;
+using Mc2.CrudTest.Domain.Entities;
+using Mc2.CrudTest.Application.Common.Interfaces;
+using Mc2.CrudTest.Application.Common.Exceptions;
 
 namespace Mc2.CrudTest.Application.Administration.Customers.Commands
 {
-    internal class DeleteCustomerCommand
+    #region command
+
+    public class DeleteCustomerCommand : IRequest
     {
+        public int Id { get; set; }
     }
+
+    #endregion;
+
+    #region handler
+
+    public class DeleteCustomerCommandHandler : IRequestHandler<DeleteCustomerCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public DeleteCustomerCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Customers.FindAsync(request.Id);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Customer), request.Id);
+            }
+
+            _dbContext.Customers.Remove(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+
+    #endregion;
 }
