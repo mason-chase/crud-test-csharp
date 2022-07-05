147f85eba370c67221444709f2ccc42b4b041ad6
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-07-05 14:10:04 +0430

    Adding bank account number validator and completing create customer command

--- src/Mc2.CrudTest.Application/Administration/Customers/Commands/CreateCustomerCommand.cs
+++ src/Mc2.CrudTest.Application/Administration/Customers/Commands/CreateCustomerCommand.cs
@@ -1,12 +1,132 @@
 ﻿using System;
-using System.Collections.Generic;
+using MediatR;
 using System.Linq;
-using System.Text;
+using FluentValidation;
+using System.Threading;
 using System.Threading.Tasks;
+using Mc2.CrudTest.Domain.Entities;
+using Mc2.CrudTest.Application.Common.Interfaces;
+using Mc2.CrudTest.Application.Common.Validators;
 
 namespace Mc2.CrudTest.Application.Administration.Customers.Commands
 {
-    internal class CreateCustomerCommand
+    #region command
+
+    public class CreateCustomerCommand : IRequest<int>
     {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BankAccountNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
     }
+
+    #endregion;
+
+    #region validator
+
+    public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public CreateCustomerCommandValidator(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .MaximumLength(36)
+                .Must(UniqueFirstName).WithMessage("'{PropertyName}' is duplicated.")
+                .WithName("First Name");
+
+            RuleFor(x => x.Lastname)
+                .NotEmpty()
+                .MaximumLength(36)
+                .Must(UniqueLastname).WithMessage("'{PropertyName}' is duplicated.")
+                .WithName("Last Name");
+
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty()
+                .MaximumLength(15)
+                .ValidPhoneNumber()
+                .Must(UniquePhoneNumber).WithMessage("'{PropertyName}' is duplicated.")
+                .WithName("Phone Number");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .MaximumLength(320)
+                .ValidEmailAddress()
+                .Must(UniqueEmail).WithMessage("'{PropertyName}' is duplicated.")
+                .WithName("Email");
+
+            RuleFor(x => x.BankAccountNumber)
+                .NotEmpty()
+                .MinimumLength(9)
+                .MaximumLength(18)
+                .ValidBankAccountNumber()
+                .Must(UniqueBankAccountNumber).WithMessage("'{PropertyName}' is duplicated.")
+                .WithName("Bank Account Number");
+        }
+
+        private bool UniqueFirstName(string firstName)
+        {
+            return !_dbContext.Customers.Any(x => x.FirstName == firstName);
+        }
+
+        private bool UniqueLastname(string lastName)
+        {
+            return !_dbContext.Customers.Any(x => x.Lastname == lastName);
+        }
+
+        private bool UniqueEmail(string email)
+        {
+            return !_dbContext.Customers.Any(x => x.Email == email);
+        }
+
+        private bool UniquePhoneNumber(string phoneNumber)
+        {
+            return !_dbContext.Customers.Any(x => x.PhoneNumber == phoneNumber);
+        }
+
+        private bool UniqueBankAccountNumber(string bankAccountNumber)
+        {
+            return !_dbContext.Customers.Any(x => x.BankAccountNumber == bankAccountNumber);
+        }
+    }
+
+    #endregion;
+
+    #region handler
+
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, int>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public CreateCustomerCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Customer
+            {
+                Email = request.Email,
+                Lastname = request.Lastname,
+                FirstName = request.FirstName,
+                PhoneNumber = request.PhoneNumber,
+                DateOfBirth = request.DateOfBirth,
+                BankAccountNumber = request.BankAccountNumber,
+            };
+
+            _dbContext.Customers.Add(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity.Id;
+        }
+    }
+
+    #endregion;
 }
---
+++ src/Mc2.CrudTest.Application/Common/Validators/BankAccountNumberValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using PhoneNumbers;
+using FluentValidation;
+using System.Threading.Tasks;
+using FluentValidation.Results;
+using System.Collections.Generic;
+using FluentValidation.Validators;
+using System.Text.RegularExpressions;
+
+namespace Mc2.CrudTest.Application.Common.Validators
+{
+    public class BankAccountNumberValidator<T> : PropertyValidator<T, string>, IRegularExpressionValidator
+    {
+        public string Expression => @"^\d{9,18}$";
+        public override string Name => "BankAccountNumberValidator";
+
+        public override bool IsValid(ValidationContext<T> context, string value)
+        {
+            return Regex.IsMatch(value, Expression);
+        }
+
+        protected override string GetDefaultMessageTemplate(string errorCode)
+        {
+            return "'{PropertyName}' is not a valid bank account number. account number length varies from 9 digits to 18 digits.";
+        }
+    }
+
+    public static class BankAccountNumberValidatorExtensions
+    {
+        public static IRuleBuilderOptions<T, string> ValidBankAccountNumber<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            var validator = (PropertyValidator<T, string>)new BankAccountNumberValidator<T>();
+
+            return ruleBuilder.SetValidator(validator);
+        }
+    }
+}
