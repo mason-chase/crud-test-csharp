e360e6b7426f146eb4628c92b5bbcb214f33ea64
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-07-05 15:18:17 +0430

    Initializing migrations and apply them to database

---
+++ src/Mc2.CrudTest.Infrastructure/Migrations/20220705102558_init.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mc2.CrudTest.Infrastructure.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: true),
+                    Lastname = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(320)", maxLength: 320, nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    BankAccountNumber = table.Column<string>(type: "nvarchar(18)", maxLength: 18, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
