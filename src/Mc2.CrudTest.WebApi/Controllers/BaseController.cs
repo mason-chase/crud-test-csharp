e9767ade13758a1e8909e8a6a6c3c39fcee9d117
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-07-05 15:23:29 +0430

    Adding customers controller

---
+++ src/Mc2.CrudTest.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mc2.CrudTest.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        private ISender _mediator = null!;
+
+        protected ISender Mediator =>
+            _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
---
+++ src/Mc2.CrudTest.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Mc2.CrudTest.Application.Administration.Customers.Queries;
+using Mc2.CrudTest.Application.Administration.Customers.Commands;
+
+namespace Mc2.CrudTest.WebApi.Controllers
+{
+    public class CustomersController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] GetCustomersQuery request)
+        {
+            var result = await Mediator.Send(request);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> Get([FromRoute] GetCustomerQuery request)
+        {
+            var result = await Mediator.Send(request);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateCustomerCommand request)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = await Mediator.Send(request);
+
+                return Ok(userId);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(UpdateCustomerCommand request)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = await Mediator.Send(request);
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete([FromRoute] DeleteCustomerCommand request)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = await Mediator.Send(request);
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
