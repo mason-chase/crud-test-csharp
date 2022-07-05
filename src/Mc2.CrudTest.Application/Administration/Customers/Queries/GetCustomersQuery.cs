386445251822bffb23fb74f43406d6889c78f46d
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-07-05 13:40:26 +0430

    Adding empty customer commands and queries

---
+++ src/Mc2.CrudTest.Application/Administration/Customers/Queries/GetCustomersQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Application.Administration.Customers.Queries
+{
+    internal class GetCustomersQuery
+    {
+    }
+}
