0444281c954d026e33b22a8b14b8a36f031efaca
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-07-05 14:41:42 +0430

    Adding connection string and cleaning web api project

--- src/Mc2.CrudTest.WebApi/Program.cs
+++ src/Mc2.CrudTest.WebApi/Program.cs
@@ -1,11 +1,11 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Mc2.CrudTest.WebApi
 {
