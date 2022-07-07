fd49984360a85e06b83e35d7d911de2df1fe32c2
Taher <taherfattahi11@gmail.com>
2022-07-07 20:14:43 +0430

    email spam detector unit test with lbfgs logistic regression algorithm and ML.NET

---
+++ Mc2.CrudTest.SpamDetector_LbfgsLogisticRegression/EmailSpamDetectorClass.cs
@@ -0,0 +1,22 @@
+﻿using Mc2_CrudTest_SpamDetector_LbfgsLogisticRegression;
+
+namespace Mc2.CrudTest.SpamDetector_LbfgsLogisticRegression
+{
+    public class EmailSpamDetectorClass
+    {
+        public static string EmailSpamDetectorValidator(string emailWord)
+        {
+            //Load sample data
+            var sampleData = new MLModel1.ModelInput()
+            {
+                Col1 = @emailWord,
+            };
+
+            //Load model and predict output
+            var result = MLModel1.Predict(sampleData);
+            
+            return result.PredictedLabel;
+        }
+       
+    }
+}
